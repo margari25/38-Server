9536aa0b0fc3f68fd1c00feb24bd704016dd530a
margari25 <budryte.m@mail.ru>
2022-06-16 13:25:39 +0300

    file types

---
+++ config.js
@@ -0,0 +1,45 @@
+// node ./index.js                      //dev
+// NODE_ENV=dev node ./indexedDB.js     //dev
+// NODE_ENV=prod node ./index.js        //prod
+// NODE_ENV=test node ./index.js        //test
+// NODE_ENV=kazkas node ./index.js      //kazkas
+
+
+const config = {};
+
+config.dev = {
+    name: 'dev',
+    httpPort: 6969,
+    passwordLength: 2,
+    defaultLanguage: 'en',
+    languages: ['en', 'lt', 'ee'],
+    db: {
+        user: 'root',
+        password: 'admin',
+        database: 'batai',
+    },
+}
+
+config.prod = {
+    name: 'prod',
+    httpPort: 42069,
+    passwordLength: 12,
+    defaultLanguage: 'lt',
+    languages: ['en', 'lt'],
+    db: {
+        user: 'node_batai_user',
+        password: 'r84tr5s25e84rrg52f5er84r5ert8r4g55e',
+        database: 'batai-r5fe1d15',
+    },
+}
+
+const nodeEnv = process.env.NODE_ENV;
+const env = nodeEnv ? nodeEnv : 'dev';
+const options = config[env] ? config[env] : config.dev;
+
+
+console.log('kur dirba kodas?');
+console.log(nodeEnv); //kur dirba kodas?
+console.log(options);
+
+export default options;
--- index.js
+++ index.js
@@ -1,47 +1,24 @@
-// node ./index.js                      //dev
-// NODE_ENV=dev node ./indexedDB.js     //dev
-// NODE_ENV=prod node ./index.js        //prod
-// NODE_ENV=test node ./index.js        //test
-// NODE_ENV=kazkas node ./index.js      //kazkas
+import { server } from './lib/server.js';
 
+const app = {};
 
-const config = {};
+app.init = () => {
+    // susikurti busimus reikiamus folder'ius
+    // susikurti busimus reikiamus failus, pvz.: robots.txt
 
-config.dev = {
-    name: 'dev',
-    httpPort: 6969,
-    passwordLength: 2,
-    defaultLanguage: 'en',
-    languages: ['en', 'lt', 'ee'],
-    db: {
-        user: 'root',
-        password: 'admin',
-        database: 'batai',
-    },
+    // prisijungti prie duomenu bazes (DB)
+
+    // paleidziam pacio serverio logika (perduodame prisijungima prie DB)
+
+    server.init();
+
+    // paleidzia papildomus nuolatinius procesus:
+    // - issitrinti nebereikalingus failus
+    // - suarchivuoti retai naudojamus failus
+    // - prasukti reikiamus API (3rd-party info, pvz.: valiutu kursai, orai)
+    // - pasitikrinti, jog vartotojai, kurie neturi teises buti prisijunge, jog ir nera prisijunge, o jei yra - atjungiame!!!
 }
 
-config.prod = {
-    name: 'prod',
-    httpPort: 42069,
-    passwordLength: 12,
-    defaultLanguage: 'lt',
-    languages: ['en', 'lt'],
-    db: {
-        user: 'node_batai_user',
-        password: 'r84tr5s25e84rrg52f5er84r5ert8r4g55e',
-        database: 'batai-r5fe1d15',
-    },
-}
+app.init();
 
-const nodeEnv = process.env.NODE_ENV;
-const env = nodeEnv ? nodeEnv : 'dev';
-const options = config[env] ? config[env] : config.dev;
-
-
-console.log('kur dirba kodas?');
-console.log(nodeEnv); //kur dirba kodas?
-console.log(options);
-// console.log('Ka parasiau terminale:', nodeEnv);
-// console.log('Kokia aplinka turesiu paleisti:', env);
-
-// export default options;
+export default app;
