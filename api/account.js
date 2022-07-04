123c2b0da584bc78790b0f9e4c012555a337ca70
margari25 <budryte.m@mail.ru>
2022-07-04 22:01:27 +0300

    server update

--- api/account.js
+++ api/account.js
@@ -88,6 +88,9 @@ handler._innerMethods.post = async (data, callback) => {
 
     delete payload.pass;
     payload.hashedPassword = utils.hash(pass)[1];
+    payload.lastLoginDate = 0;
+    payload.registrationData = Date.now();
+    payload.browser = data.user.browser;
 
     const [createErr] = await file.create('accounts', email + '.json', payload);
     if (createErr) {
@@ -163,10 +166,7 @@ handler._innerMethods.put = async (data, callback) => {
         });
     }
 
-    // cia kazkur klaida: start
     const { fullname, pass } = payload;
-
-    console.log(fullname, pass);
 
     if (fullname) {
         const [fullnameErr, fullnameMsg] = IsValid.fullname(fullname);
@@ -185,7 +185,6 @@ handler._innerMethods.put = async (data, callback) => {
             });
         }
     }
-    // cia kazkur klaida: end
 
     const [readErr, readMsg] = await file.read('accounts', email + '.json');
     if (readErr) {
@@ -201,8 +200,12 @@ handler._innerMethods.put = async (data, callback) => {
         });
     }
 
-    userData.fullname = fullname;
-    userData.pass = pass;
+    if (fullname) {
+        userData.fullname = fullname;
+    }
+    if (pass) {
+        userData.hashedPassword = utils.hash(pass)[1];
+    }
 
     const [updateErr] = await file.update('accounts', email + '.json', userData);
 
@@ -218,9 +221,29 @@ handler._innerMethods.put = async (data, callback) => {
 }
 
 // DELETE
-handler._innerMethods.delete = (data, callback) => {
+handler._innerMethods.delete = async (data, callback) => {
+    // 1) suzinoti apie kuri vartotoja norima gauti duomenis
+    const email = data.searchParams.get('email');
+
+    // 2) Patikriname ar gautas email yra email formato
+    const [emailErr, emailMsg] = IsValid.email(email);
+    if (emailErr) {
+        return callback(400, {
+            msg: emailMsg,
+        });
+    }
+
+    // 3) Trinam paskyra
+    const [deleteErr] = await file.delete('accounts', email + '.json', userData);
+
+    if (deleteErr) {
+        return callback(500, {
+            msg: 'Nepavyko istrinti paskyros informacijos, del vidines serverio klaidos',
+        });
+    }
+
     return callback(200, {
-        msg: 'Account: delete',
+        msg: 'Paskyra istrinta sekmingai',
     });
 }
 
